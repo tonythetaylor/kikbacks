0d112993fb28fa3eb41d2f0f5798266ca746e66b
Anthony Taylor <ant.taylor2@gmail.com>
2024-01-29 18:01:32 -0500

    updated home page image

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -34,7 +34,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           </div>
 
           <Image 
-            src="/assets/images/hero.png"
+            src="/assets/images/hero2.png"
             alt="hero"
             width={1000}
             height={1000}
